3635b22716dd9a6769c29c38634df5534056c3d0
Lukáš Koliandr <koliandr.lu@gmail.com>
2023-11-25 17:12:04 +0100

    feat: sprites

--- Assets/Scripts/Candidate.cs
+++ Assets/Scripts/Candidate.cs
@@ -3,30 +3,37 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-
-public class Candidate : MonoBehaviour {
+public class Candidate : MonoBehaviour
+{
     [SerializeField]
     private ProgressBar authenticityBar;
-    
-    private PropertiesManager propertiesManager = GameObject.FindGameObjectWithTag("logic").GetComponent<PropertiesManager>();
+
+    private PropertiesManager propertiesManager
+        = GameObject.FindGameObjectWithTag("logic").GetComponent<PropertiesManager>();
 
     public Property[] GoodProperties { get; private set; }
     public Property[] BadProperties { get; private set; }
     public SpecialSkill SpecialSkill { get; private set; }
 
-
     private const int maxAuthenticity = 100;
-    public int Authenticity { get => (int)authenticityBar.Value; }
+    public int Authenticity
+    {
+        get => (int)authenticityBar.Value;
+    }
 
-    public void ChangeAuthenticity(int deltaAuthenticity) {
+    public void ChangeAuthenticity(int deltaAuthenticity)
+    {
         authenticityBar.Value = Mathf.Clamp(deltaAuthenticity, 0, maxAuthenticity);
-        if (Authenticity <= maxAuthenticity / 10) {
+        if (Authenticity <= maxAuthenticity / 10)
+        {
             // auto lose game
         }
     }
 
-    public void GenerateNewCandidate() {
+    public void GenerateNewCandidate()
+    {
         // generate new properties and special skill
+        propertiesManager.ResetProperties();
         GoodProperties = propertiesManager.GetCandidateProperties(good: true);
         BadProperties = propertiesManager.GetCandidateProperties(good: false);
         SpecialSkill = propertiesManager.GetSpecialSkill();
@@ -35,7 +42,8 @@ public class Candidate : MonoBehaviour {
         authenticityBar.Value = maxAuthenticity / 2;
     }
 
-    void Start() {
+    void Start()
+    {
         authenticityBar.Max = maxAuthenticity;
         GenerateNewCandidate();
     }
