cdf5d38751b367f8ea18832173ebff0ae91b7eed
Couleslaw <smolik.jakub73@gmail.com>
2023-11-26 11:51:58 +0100

    update debate manager

--- Assets/Scripts/Managers/DebageManager.cs
+++ Assets/Scripts/Managers/DebageManager.cs
@@ -7,6 +7,9 @@ public class DebageManager : MonoBehaviour
     private Question[] _generalQuestions;
     private Question[] _playerQuestions;
     private Question[] _enemyQuestions;
+    private Question[] _questions;
+
+    private int _questionNum = 0;
 
     public int PlayerAuthenticity => _player.Authenticity;
     public int EnemyAuthenticity => _enemy.Authenticity;
@@ -21,26 +24,46 @@ public class DebageManager : MonoBehaviour
     private GameObject _enemyObject;
     private Candidate _enemy;
 
-
     void Start()
     {
         _player = _playerObject.GetComponent<Candidate>();
         _enemy = _enemyObject.GetComponent<Candidate>();
     }
 
-    public void SetUpQuestion() {
+    public void SetUpQuestions() {
         // TOHLE SE NEMUZE VOLAT VE STARTU, PAK JE NULL REFERENCE EXEPTION
 
         _generalQuestions = QuestionLoader.GetRandomQuestions();
         _playerQuestions = QuestionLoader.GetQuestionsForCandidate(_player);
         _enemyQuestions = QuestionLoader.GetQuestionsForCandidate(_enemy);
+        _questions = new Question[] {
+            _generalQuestions[0],
+            _playerQuestions[0],
+            _enemyQuestions[0],
 
-        Debug.Log(_playerQuestions[0].Text);
-        Debug.Log(_enemyQuestions[0].Text);
-        Debug.Log(_generalQuestions[0].Text);
+            _generalQuestions[1],
+            _playerQuestions[1],
+            _enemyQuestions[1],
+            
+            _generalQuestions[2],
+            _playerQuestions[2],
+            _enemyQuestions[2],
+            
+            _generalQuestions[3],
+        };
     }
 
-    public void AskAnotherQuestion() {
+    /*
+    bool ready = false;
+    public void Update() {
+        if (!ready) {
+            SetUpQuestions();
+            ready = true;
+        }
+    }
+    */
 
+    public void AskAnotherQuestion() {
+        
     }
 }
